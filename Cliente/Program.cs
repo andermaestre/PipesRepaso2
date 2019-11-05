eb54edb628a3a6693264e11bd5f665aac5799c70
Ander Maestre <andermaestre_90@hotmail.es>
2019-11-05 19:19:01 +0100

    Hecho

---
+++ Cliente/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
+using System.Diagnostics;
+
+namespace Cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int debe, haber, importe;
+            String pal;
+
+            NamedPipeClientStream pipa = new NamedPipeClientStream(".","pipazo", PipeDirection.InOut);
+            pipa.Connect();
+            StreamWriter sw = new StreamWriter(pipa);
+            sw.AutoFlush = true;
+            StreamReader sr = new StreamReader(pipa);
+
+            debe=int.Parse(sr.ReadLine());
+            haber = int.Parse(sr.ReadLine());
+            pal = sr.ReadLine();
+            while (pal.CompareTo("fin")!=0)
+            {
+                importe = int.Parse(sr.ReadLine());
+                if (pal.CompareTo("ingreso")==0)
+                {
+                    haber += importe;
+                }
+                else
+                {
+                    debe += importe;
+                }
+                pal = sr.ReadLine();
+            }
+            sw.WriteLine(debe);
+            sw.WriteLine(haber);
+            pipa.WaitForPipeDrain();
+            pipa.Close();
+        }
+    }
+}
---
+++ Repaso/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
+using System.Diagnostics;
+
+
+namespace Repaso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String pal;
+            int importe;
+            int resdebe, reshaber;
+            int debe, haber;
+            NamedPipeServerStream pipa = new NamedPipeServerStream("pipazo", PipeDirection.InOut);
+            pipa.WaitForConnection();
+            StreamWriter sw = new StreamWriter(pipa);
+            sw.AutoFlush = true;
+            StreamReader sr = new StreamReader(pipa);
+            
+            Console.WriteLine("Debe: ");
+            debe = int.Parse(Console.ReadLine());
+            Console.WriteLine("Haber: ");
+            haber = int.Parse(Console.ReadLine());
+            sw.WriteLine(debe);
+            sw.WriteLine(haber);
+            
+            pal = pideyval();
+
+            while (pal.CompareTo("fin") != 0)
+            {
+                Console.WriteLine("Importe: ");
+                importe = int.Parse(Console.ReadLine());
+                sw.WriteLine(pal);
+                sw.WriteLine(importe);
+                pal = pideyval();
+            }
+            sw.WriteLine("fin");
+            resdebe = int.Parse(sr.ReadLine());
+            reshaber = int.Parse(sr.ReadLine());
+            Console.WriteLine("Saldo = {0}", reshaber - resdebe);
+
+
+
+            Console.ReadLine();
+            pipa.Close();
+        }
+
+        private static string pideyval()
+        {
+            String pal;
+            do
+            {
+                Console.WriteLine("Escribe: ingreso, reintegro o fin");
+                pal = Console.ReadLine().ToLower();
+            } while (pal.CompareTo("ingreso") != 0 && pal.CompareTo("reintegro") != 0 && pal.CompareTo("fin") != 0);
+            return pal;
+        }
+    }
+}
